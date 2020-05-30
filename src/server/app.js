ac038cca3007e1fc5888345af203e83720f7b426
mikenthiwa <mikenthiwa@gmail.com>
2020-05-30 15:02:38 +0300

    feat(categories and jokes):  - user should be able to request data from categoris and jokse

--- src/server/app.js
+++ src/server/app.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import schema from '../schema';
+import cors from 'cors';
 import {ApolloServer} from "apollo-server-express";
 import typeDefs from "../schema";
 import resolvers from "../resolvers";
 
 export default () => {
     const app = express();
+    app.use(cors());
     const server = new ApolloServer({ typeDefs, resolvers});
     server.applyMiddleware({ app });
     return app
